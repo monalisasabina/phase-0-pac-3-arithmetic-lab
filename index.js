36b3826eee5acc90ba2f540517c3342c82c03f32
Monalisa Onyango <+>
2024-06-22 16:20:36 +0300

    pac3 arithmetic

---
+++ index.js
@@ -0,0 +1,52 @@
+function add() {
+    // we'll fill this in shortly
+  }
+
+function subtract(){
+
+  } 
+
+function multiply(){
+
+}  
+
+function divide(){
+
+}
+
+function add() {
+    return "something";
+  }
+
+function add(a, b) {
+    return a + b;
+  }
+
+function subtract(a, b){
+    return a - b;
+  }
+
+function multiply(a, b){
+  return a * b;
+  }
+
+function divide(a, b){
+  return a / b;
+  }
+
+ //INCREMENT DECREMENT 
+function increment(n){
+    return ++n;
+}
+
+function decrement(n){
+    return --n;
+}
+
+function makeInt(n){
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
